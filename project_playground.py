a4e79380e95dcf50c9f66dad2d36d4ecdb11b206
noahn <noahn@nobodywhocares.org>
2020-12-02 15:18:29 -0700

    name changes

--- project_playground.py
+++ project_playground.py
@@ -7,8 +7,10 @@ import pytesseract
 import cv2 as cv
 import numpy as np
 
+
 # loading the face detection classifier
 face_cascade = cv.CascadeClassifier('readonly/haarcascade_frontalface_default.xml')
+
 
 # the rest is up to you!
 def get_images(zip_name, debug = False):
